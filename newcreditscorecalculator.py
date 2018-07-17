92320b5db09bd181f6a0bcedd4c66ba6dcbcd150
arthurleemartinez <40931653+arthurleemartinez@users.noreply.github.com>
2018-07-17 01:22:18 -0500

    Update newcreditscorecalculator.py
    Changed variable scopes and added a new function, one step closer to intuitive working calculator

--- newcreditscorecalculator.py
+++ newcreditscorecalculator.py
@@ -8,11 +8,12 @@
 # newCreditCardPayments = int(input("How much have you paid or intend to pay towards your credit card bills?"))
 # numberOfInquiries = int(input("How many inquiries will you make in the next 2 years?"))
 
+# global variables start here
 belowAverage = 0.25
 average = .50
 good = .75
 excellent = 1
-
+ccR = currentScore 
 currentDebt = currentCarNote + currentCardBalances
 newCreditHistoryLength = currentCreditHistoryLength + monthsPassed
 oldDebt = currentCarNote + currentCardBalance
@@ -20,24 +21,32 @@ newCarNote = currentCarNote - carPaymentMonthly
 monthsPassed = 24
 numberOfInquiries = 0
 currentScore = 625
-currentScoreCoefficient = csc
 monthsPassed = 24
 currentCarNote = 11126
 currentCardBalance = 687
+b = newPaymentHistory
+# this is a integer in a range of 0-1 based on percentage of max credit score you have
+zeroOne = (cc - 300) / 850
 newCreditCardPayments = 76
 levelOfDebt = newDebt(oldDebt)
+# this will be a percentage converted to a range of 0-1
+newPaymentHistory = 1 
 
 def newDebt(oldDebt):
     z = monthsPassed * carPaymentMonthly
     zz = currentCardBalance - newCreditCardPayments + (currentCarNote - z)
     return zz
-    
+
+def changeLetter(newPaymentHistory, levelOfDebt, newCreditCardHistoryLength, ine):
+    if newPaymentHistory > 0.99:
+        bR
+
 # this will be a percentage converted to a range of 0-1
 newPaymentHistory = 1 
 # this is a function that takes variables and calculates a new theoretical score
 def ncscalculator(currentScore, newDebt): 
     cc = currentScore
-    b = newPaymentHistory
+    
     n = newCreditHistoryLength
     l = levelOfDebt
     ch = newCreditCardHistoryLength
@@ -52,7 +61,8 @@ def ncscalculator(currentScore, newDebt):
     inin = ine * 0.10
     # payment history is out of 1
     newPaymentHistory = 1
-    newScore = cc * (0.1 + (bb + ll + chch + inin))
-  
+    # this is a variable that gets your number out 1 to multiple by 550 to add to 300 to get newScore
+    ns = (zeroOne * (0.1 + (bb + ll + chch + inin))
+    newScore = (ns * 550) + 300
 print newDebt(oldDebt)
 print ncscalculator(625, newDebt(oldDebt)) 
